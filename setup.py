f161d3f9d80f3d2a254de13e4e2e3fab0f4080c5
IAMCB <IAMCB@Teck90.ca>
2019-01-29 20:24:10 -0500

    updated package to include bin for script

--- setup.py
+++ setup.py
@@ -8,10 +8,14 @@ setuptools.setup(
     description='CLI app to create Finder aliases in OS X',
     url='https://github.com/iamcb/mkalias',
     packages=['mkalias'],
-    scripts=['bin/mkalias.py'],
+    # scripts=['bin/mkalias.py'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: MacOS :: MacOS X",
     ],
+    entry_points={
+        'console_scripts':
+            ['mkalias = mkalias.mkalias:main', ],
+    }
 )
