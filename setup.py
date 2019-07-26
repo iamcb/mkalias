db0b9319d764abfb2ee838823fec94763dd44f2d
IAMCB <IAMCB@teck90.ca>
2019-07-25 21:19:13 -0400

    updated packages line

--- setup.py
+++ setup.py
@@ -15,8 +15,7 @@ setuptools.setup(
     description='CLI app to create Finder aliases in OS X',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    # packages=setuptools.find_packages(),
-    packages=['mkalias_foss'],
+    packages=setuptools.find_packages(include=['mkalias-foss', ]),
     setup_requires=['setuptools_scm', ],
     install_requires=['osascript', 'click', ],
     classifiers=[
